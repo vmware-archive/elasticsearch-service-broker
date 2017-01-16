d5e6e1f4bd83c941d1fcc08a2f840a14d25decc0
Navjot Sagoo <nsagoo@pivotal.io>
2017-01-16 14:57:51 -0500

    update uri to return hostname and port

--- src/main/java/io/pivotal/cf/broker/es/ElasticSearchBroker.java
+++ src/main/java/io/pivotal/cf/broker/es/ElasticSearchBroker.java
@@ -159,7 +159,7 @@ class ElasticSearchBroker extends DefaultServiceImpl {
             m.put(INDEX_NAME_KEY, instance.getParameters().get(INDEX_NAME_KEY));
             m.put("host", env.getProperty("ELASTIC_HOST"));
             m.put("port", env.getProperty("ELASTIC_PORT"));
-            String uri = "elastic://" + m.get("host") + "/" + m.get(INDEX_NAME_KEY);
+            String uri = "http://" + m.get("host") + ":" + m.get("port") + "/" + m.get(INDEX_NAME_KEY);
             m.put("uri", uri);
             return m;
         } catch (Throwable t) {
